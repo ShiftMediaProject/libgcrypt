9a951bffaa7e2945d7d8ee807d53724c2d1e36d7
Matthew Oliver <protogonoi@gmail.com>
2014-08-23 22:17:51 +1000

    Update VS project files for recent merge.

--- SMC/gcrypt.h
+++ SMC/gcrypt.h
@@ -63,11 +63,11 @@ extern "C" {
    return the same version.  The purpose of this macro is to let
    autoconf (using the AM_PATH_GCRYPT macro) check that this header
    matches the installed library.  */
-#define GCRYPT_VERSION "1.6.1"
+#define GCRYPT_VERSION "1.6.2"
 
 /* The version number of this header.  It may be used to handle minor
    API incompatibilities.  */
-#define GCRYPT_VERSION_NUMBER 0x010601
+#define GCRYPT_VERSION_NUMBER 0x010602
 
 
 /* Internal: We can't use the convenience macros for the multi
--- SMC/version.h
+++ SMC/version.h
@@ -1,9 +1,9 @@
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libgcrypt 1.6.1"
+#define PACKAGE_STRING "libgcrypt 1.6.2"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.6.1"
+#define PACKAGE_VERSION "1.6.2"
 
 /* Version of this package */
-#define VERSION "1.6.1"
+#define VERSION "1.6.2"
