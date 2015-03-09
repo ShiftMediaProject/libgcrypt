d8b5d35d62d265c563e0a8483050aa915181feec
Matt Oliver <protogonoi@gmail.com>
2015-03-09 15:31:35 +1100

    project: Update for upstream 1.6.3 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,11 +1,11 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.6.2"
+#define PACKAGE_VERSION "1.6.3"
 
 /* Version of this package */
-#define VERSION "1.6.2"
+#define VERSION "1.6.3"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "a973a24"
+#define BUILD_REVISION ""
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2014-08-29T15:35+0000"
+#define BUILD_TIMESTAMP "2015-03-09T03:52+0000"
