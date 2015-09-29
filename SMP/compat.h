480a63cecd571e4b3cf87ed0ce489124e3059153
Matt Oliver <protogonoi@gmail.com>
2015-09-29 23:03:27 +1000

    project: Update to support VS2015.

---
+++ SMP/compat.h
@@ -0,0 +1,39 @@
+/*
+ * Correctly setup required msvc compatibility options based on which native
+ * Windows compiler is in use.
+ * Copyright (c) 2015 Matthew Oliver
+ *
+ * This file is part of Shift Media Project.
+ *
+ * Shift Media Project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Shift Media Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the code; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef SMP_COMPAT_H
+#define SMP_COMPAT_H
+
+#ifdef _MSC_VER
+#   define inline __inline
+#   define strcasecmp _stricmp
+#   define strncasecmp _strnicmp
+#   include <crtversion.h>
+#   if _VC_CRT_MAJOR_VERSION >= 14
+#       pragma comment(lib, "legacy_stdio_definitions.lib")
+#   else
+#       define snprintf _snprintf
+#       define strtoll _strtoi64
+#   endif
+#endif
+
+#endif /* SMP_COMPAT_H */
--- SMP/sys/time.h
+++ SMP/sys/time.h
@@ -1,10 +1,30 @@
+/*
+ * MSVC sys/time.h compatability header.
+ * Copyright (c) 2015 Matthew Oliver
+ *
+ * This file is part of Shift Media Project.
+ *
+ * Shift Media Project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Shift Media Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the code; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
-#ifndef _SYS_TIME_H_
-#define _SYS_TIME_H_
+#ifndef _SMP_SYS_TIME_H_
+#define _SMP_SYS_TIME_H_
 
 #ifndef _MSC_VER
-#error "Use this header only with Microsoft Visual C++ compilers!"
-#endif
+#   include_next <sys/time.h>
+#else
 
 #include <time.h>
 #include <winsock2.h>
@@ -43,4 +63,6 @@ static __inline int gettimeofday(struct timeval * tp, struct timezone * tzp)
     return 0;
 }
 
-#endif
+#endif /* _MSC_VER */
+
+#endif /* _SMP_SYS_TIME_H_ */
