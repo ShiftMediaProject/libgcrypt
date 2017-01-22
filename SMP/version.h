d60493246ec76168ff3f04b891baf75b5e7451e3
Matt Oliver <protogonoi@gmail.com>
2017-01-22 22:27:23 +1100

    project: Update for 1.7.6 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,11 +1,11 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.7.5"
+#define PACKAGE_VERSION "1.7.6"
 
 /* Version of this package */
-#define VERSION "1.7.5"
+#define VERSION "1.7.6"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "89ec6c1"
+#define BUILD_REVISION "64e4808"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2016-12-19T02:48+0000"
+#define BUILD_TIMESTAMP "2017-01-22T11:19+0000"
