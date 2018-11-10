e1214d008af157dabbf6113e4464770dc0f61df6
Matt Oliver <protogonoi@gmail.com>
2018-11-10 22:00:26 +1100

    project: Update for 1.8.4 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,11 +1,11 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.8.3"
+#define PACKAGE_VERSION "1.8.4"
 
 /* Version of this package */
-#define VERSION "1.8.3"
+#define VERSION "1.8.4"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "5600d2d"
+#define BUILD_REVISION "9377517"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2018-06-17T12:43+0000"
+#define BUILD_TIMESTAMP "2018-11-10T10:56+0000"
