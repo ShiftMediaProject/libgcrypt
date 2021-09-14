df1a3c62634dd0dcf5798ae375ad179e757a250f
Matt Oliver <protogonoi@gmail.com>
2021-09-14 16:54:16 +1000

    project: Update for 1.9.4 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,11 +1,11 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.9.3"
+#define PACKAGE_VERSION "1.9.4"
 
 /* Version of this package */
-#define VERSION "1.9.3"
+#define VERSION "1.9.4"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "cb78627"
+#define BUILD_REVISION "05422ca"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2021-04-20T13:14+0000"
+#define BUILD_TIMESTAMP "2021-09-14T06:46+0000"
