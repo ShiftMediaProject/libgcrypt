bb095279d19f3fb54ca4c37231cf37e947d9948a
Matt Oliver <protogonoi@gmail.com>
2022-02-13 21:00:53 +1100

    project: Update for 1.10.0 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,11 +1,11 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.9.4"
+#define PACKAGE_VERSION "1.10.0"
 
 /* Version of this package */
-#define VERSION "1.9.4"
+#define VERSION "1.10.0"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "05422ca"
+#define BUILD_REVISION "e4ab214"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2021-09-14T06:46+0000"
+#define BUILD_TIMESTAMP "2022-02-13T08:39+0000"
