8eef3cda62413b98e68a2c9549eaeaec54b4c22f
Matt Oliver <protogonoi@gmail.com>
2023-12-24 00:05:48 +1100

    project: Update for 1.10.3 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,11 +1,11 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.10.2"
+#define PACKAGE_VERSION "1.10.3"
 
 /* Version of this package */
-#define VERSION "1.10.2"
+#define VERSION "1.10.3"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "1c5cbac"
+#define BUILD_REVISION "aa16108"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2023-05-20T16:04+0000"
+#define BUILD_TIMESTAMP "2023-12-23T13:00+0000"
