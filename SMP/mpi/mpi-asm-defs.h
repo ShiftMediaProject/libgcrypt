60b4fb1107b49a0a112b39517c63a020c2a882a3
Matt Oliver <protogonoi@gmail.com>
2024-02-03 20:31:36 +1100

    project: Update mpi to for fix 64bit error.

--- SMP/mpi/mpi-asm-defs.h
+++ SMP/mpi/mpi-asm-defs.h
@@ -1,7 +1,11 @@
 /* This file defines some basic constants for the MPI machinery.  We
  * need to define the types on a per-CPU basis, so it is done with
  * this file here.  */
+#if (__GNUC__ >= 3 && defined(__x86_64__) && defined(__ILP32__)) || (defined(_MSC_VER) && (defined(__x86_64__) || defined(__x86_64) || defined(_M_X64)))
+#define BYTES_PER_MPI_LIMB 8
+#else
 #define BYTES_PER_MPI_LIMB  (SIZEOF_UNSIGNED_LONG)
+#endif
 
 
 
